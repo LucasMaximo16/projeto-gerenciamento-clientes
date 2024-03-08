c08dd5d46fdb03bf034213e504c4112df90ffba3
Lucas <lucasmaximodeandrade@gmail.com>
2024-03-08 19:40:15 -0300

    Alteracao

--- gerenciamento-clientes_frontEnd/src/utils/Formik/register.ts
+++ gerenciamento-clientes_frontEnd/src/utils/Formik/register.ts
@@ -3,8 +3,8 @@ import * as Yup from 'yup';
 const registerInitialValues = { nome: '', email: '', telefone: '', coordenada_x: '', coordenada_y: '' };
 const registerSchema = Yup.object().shape({
     nome: Yup.string(),
-    email: Yup.string().email('Email Invalido').min(8, 'Email Invalido').max(124, 'Email Invalido'),
-    telefone: Yup.string().min(9,'Telefone Invalido').max(12,"Telefone Invalido"),
+    email: Yup.string().email('Email Invalido'),
+    telefone: Yup.string(),
     coordenada_x: Yup.number().max(20,"Valor Maximo é 20"),
     coordenada_y: Yup.number().max(20, "Valor Maximo é 20")
 });
